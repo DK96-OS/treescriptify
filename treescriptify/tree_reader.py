5972eec2663724bbcab5cf5c960462fa0e4c5c13
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2024-03-28 20:06:41 -0400

    Treescriptify: *u script_writer.py *u tree_reader.py

--- treescriptify/tree_reader.py
+++ treescriptify/tree_reader.py
@@ -1,12 +1,18 @@
 """
 """
+import json
 from typing import Generator
 
 from treescriptify.tree_node_data import TreeNodeData
 
 
-def generate_from_json(json: str) -> Generator[TreeNodeData, None, None]:
+def generate_from_json(json_string: str) -> Generator[TreeNodeData, None, None]:
     """
     """
-    #todo:
+    tree_son, report = json.loads(json_string)
+    tree_son = tree_son['contents']
+    depth = 0
+    # todo:
+    for i in tree_son:
+
     pass
