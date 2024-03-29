91cc6ad7f26c3f3e02a279f2e27be9607cf686c2
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2024-03-28 20:29:30 -0400

    Treescriptify: *u tree_runner.py

--- treescriptify/tree_runner.py
+++ treescriptify/tree_runner.py
@@ -1,12 +1,34 @@
 """Tree Runner
 """
+import subprocess
 
 from input.input_data import InputData
 
 
 def get_tree_json(data: InputData) -> str:
+    """Obtain the Tree Information as a JSON string.
     """
+    result = subprocess.run(
+        args=['tree', '-J', '-i', '-x', '--noreport'] + _check_arguments(data),
+        capture_output=True,
+        text=True,
+        shell=False,
+    )
+    output = result.stdout
+    #error = result.stderr
+    return output
+
+
+def _check_arguments(data: InputData) -> list[str]:
+    """Check the Input Data and map flags to tree command.
     """
-    #todo:
-    pass
-    return ""
+    extras = []
+    if data.directories_only:
+        extras += '-d'
+    if data.prune_dirs:
+        extras += '--prune'
+    if data.include_hidden:
+        extras += '-a'
+    if data.git_ignore:
+        extras += '--gitignore'
+    return extras
