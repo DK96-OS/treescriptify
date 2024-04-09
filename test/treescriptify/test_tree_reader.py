853bc4d93068406e6fea2cfac6ad2bb9e192afcd
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2024-04-09 17:53:58 -0400

    Test: *u test_tree_reader.py - debug test setup, add test cases

--- test/treescriptify/test_tree_reader.py
+++ test/treescriptify/test_tree_reader.py
@@ -7,11 +7,15 @@ from treescriptify.tree_reader import generate_from_json
 
 
 def wrap_root_dir(inner_dirs: str) -> str:
-	return '{"type":"directory", "name":"src", "contents":[' + inner_dirs + ']}'
+	return '{"type":"directory", "name":".", "contents":[' + inner_dirs + ']}'
 
 
 def get_src_dir() -> str:
 	return '{"type":"directory", "name":"src", "contents":[]}'
+
+
+def get_file(name: str) -> str:
+	return '{"type":"file","name":"' + name + '"}'
 
 
 def test_generate_from_json_empty_dir_returns_empty_list():
@@ -27,7 +31,8 @@ def test_generate_from_json_src_dir_returns_tree_node():
 @pytest.mark.parametrize(
     'test_input,expected',
     [
-        ('{"type":"directory", "name":"src", "contents":[]}', [TreeNodeData(0, True, 'src')]),
+        (wrap_root_dir(get_file('build.file')), [TreeNodeData(0, False, 'build.file')]),
+        (wrap_root_dir(get_file('build.file') + ',\n\t\t' + get_file('req.txt')), [TreeNodeData(0, False, 'build.file'), TreeNodeData(0, False, 'req.txt')]),
     ]
 )
 def test_generate_from_json_(test_input, expected):
