7a75a20a8509b0315f50b1674ea745ba36341a93
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2024-04-09 18:40:10 -0400

    Test: *u test_tree_runner.py - add test cases

--- test/treescriptify/test_tree_runner.py
+++ test/treescriptify/test_tree_runner.py
@@ -1,15 +1,29 @@
 """Testing Module Methods
 """
 import pytest
+import subprocess
 
 from input.input_data import InputData
 from treescriptify.tree_runner import _check_arguments, get_tree_json
 
 
+def get_cmd_result(*args, **kwargs):
+	result = type('MyObject', (object,), {'stdout': '[{"type":"directory","name":".","contents":[{"type":"file","name":"test_script_writer.py"},{"type":"file","name":"test_tree_reader.py"},{"type":"file","name":"test_tree_runner.py"}]}]'})
+	return result
+
+
 def test_get_tree_json_():
 	input_data = InputData()
-	#assert get_tree_json(input_data)
-	pass
+	with pytest.MonkeyPatch().context() as c:
+		c.setattr(subprocess, 'run', lambda *args, **kwargs: get_cmd_result(*args, **kwargs))
+		result = get_tree_json(input_data)
+		expected_files = [
+			"test_script_writer.py",
+			"test_tree_reader.py",
+			"test_tree_runner.py",
+		]
+		for filename in expected_files:
+			assert filename in result
 
 
 @pytest.mark.parametrize(
