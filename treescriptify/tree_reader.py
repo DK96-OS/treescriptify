6633383a62b3738e5827b8112b6012af33bb788b
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2024-04-19 12:34:17 -0400

    Treescriptify: *u tree_reader.py - debug method generate_from_json

--- treescriptify/tree_reader.py
+++ treescriptify/tree_reader.py
@@ -1,6 +1,7 @@
 """
 """
 import json
+from sys import exit
 from typing import Generator
 
 from .tree_node_data import TreeNodeData
@@ -10,7 +11,13 @@ def generate_from_json(json_string: str) -> Generator[TreeNodeData, None, None]:
     """Read the JSON string and generate TreeNodeData for all elements.
     """
     full_json = json.loads(json_string)
-    for i in full_json['contents']:
+    if len(full_json) == 1:
+        dirs_dict = full_json[0]
+    elif len(full_json) < 1:
+        exit('Tree Command Failed')
+    else:
+        exit('Additional unexpected data returned from Tree Command.')
+    for i in dirs_dict['contents']:
         for node in _process_node(i, 0):
             yield node
 
