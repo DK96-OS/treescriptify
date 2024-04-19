5c99805bf724a0a33afab1e474856e60a1e42338
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-04-19 09:57:35 -0400

    Publishing:
    * Update setup.py - set version to 0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="treescriptify-this",
-    version="0.2",
+    version="0.1",
     description="Create TreeScript from an existing filesystem tree.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
