3a2201d8b3ab3c4cfb529ce378916385bd19b5c6
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-04-19 11:16:03 -0400

    Debug Publishing (#10)
    Publishing:
    * Update setup.py - fix name

--- setup.py
+++ setup.py
@@ -4,24 +4,24 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="treescriptify-this",
+    name="treescriptify",
     version="0.1",
     description="Create TreeScript from an existing filesystem tree.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author="DK96-OS",
-    url="https://github.com/DK96-OS/treescriptify-this",
+    url="https://github.com/DK96-OS/treescriptify",
     project_urls={
-        "Issues": "https://github.com/DK96-OS/treescriptify-this/issues",
-        "Source Code": "https://github.com/DK96-OS/treescriptify-this"
+        "Issues": "https://github.com/DK96-OS/treescriptify/issues",
+        "Source Code": "https://github.com/DK96-OS/treescriptify"
     },
     license="GPLv2",
     packages=find_packages(exclude=['test']),
     entry_points={
         'console_scripts': [
-            'treescriptify=treescriptify_this.__main__:main',
-            'treescriptify_this=treescriptify_this.__main__:main',
-            'treescriptify-this=treescriptify_this.__main__:main',
+            'treescriptify=treescriptify.__main__:main',
+            'treescriptify_this=treescriptify.__main__:main',
+            'treescriptify-this=treescriptify.__main__:main',
         ],
     },
     classifiers=[
