303da15444830eebbda27e6c98a3b4765ff5376b
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2024-05-08 09:55:45 -0400

    Revert "Treescriptify Test Package:"
    This reverts commit be6823b34531af86d21df1bb38b08f75286c7841.

--- test/test_windows_tree.py
+++ test/test_windows_tree.py
@@ -55,10 +55,10 @@ def mock_nested_tree(tmp_path):
 def mock_hidden_tree(tmp_path):
     github_dir = tmp_path / '.github'
     github_dir.mkdir()
+    (github_dir / 'dependabot.yml').touch()
     workflows_dir = github_dir / 'workflows'
     workflows_dir.mkdir()
     (workflows_dir / 'ci.yml').touch()
-    (github_dir / 'dependabot.yml').touch()
     (tmp_path / '.hidden.txt').touch()
     return Path(str(tmp_path))
 
