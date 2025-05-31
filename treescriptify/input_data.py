f45db90f2351fd481d4fdb8e940b6349429dbd2e
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-05-31 11:45:44 -0400

    TreeScriptify Package: * Update tree_reader - add hidden json load method to support method generate_from_json * Update input_data - update docs

--- treescriptify/input_data.py
+++ treescriptify/input_data.py
@@ -1,17 +1,19 @@
-"""Input Data to the Program
+""" Input Data to the Program
 """
 from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
 class InputData:
-    """The optional flags to modify the output of the program.
-    """
+    """ The optional flags to modify the output of the program.
 
+**Fields:**
+ - include_hidden (bool): Whether to include hidden files and directories. Default: True.
+ - directories_only (bool): Filter out all files. Default: False.
+ - git_ignore (bool): Try to use gitignore. Note, unix-tree required. No windows support yet. Default: True.
+ - prune_dirs (bool): Filter out empty directories. Default: False.
+    """
     include_hidden: bool = True
-    #
     directories_only: bool = False
-    #
     git_ignore: bool = True
-    #
     prune_dirs: bool = False
