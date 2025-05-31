4504cdbfc975b93b22d61965fb1b9a0424333128
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-05-31 11:59:23 -0400

    TreeScriptify Package: * Update __main__ - improve testability

--- treescriptify/__main__.py
+++ treescriptify/__main__.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python
-"""Main Entry Point
-"""
-import os
-from sys import argv
-
-from .argument_parser import parse_args
-from . import tsfy, tsfy_windows
 
 
 def main():
+    import os
+    from sys import argv
+    from .argument_parser import parse_args
+    from . import tsfy, tsfy_windows
     input_data = parse_args(argv[1:])
     output_data = tsfy_windows(input_data) if os.name == 'nt' else tsfy(input_data)
     print(output_data)
