f37ddadffecbc3ce8f3295b0975fde22bcfdbb3f
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-06-29 18:23:36 -0400

    Project Root: * Update setup.py - upgrade GPLv2 to GPLv3 * Update README.md - add command line options section * Update LICENSE - upgrade GPLv2 to GPLv3

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Setup Package Configuration
+""" Setup Package Configuration
 """
 from setuptools import setup, find_packages
 
@@ -15,7 +15,7 @@ setup(
         "Issues": "https://github.com/DK96-OS/treescriptify/issues",
         "Source Code": "https://github.com/DK96-OS/treescriptify"
     },
-    license="GPLv2",
+    license="GPLv3",
     packages=find_packages(exclude=['test', 'test.*']),
     entry_points={
         'console_scripts': [
@@ -30,7 +30,7 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
