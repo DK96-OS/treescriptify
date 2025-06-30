249729e4ec9323997b418fce98cc305841756705
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-06-30 13:23:23 -0400

    TreeScriptify Package Tests: * Update test_file_tree - add test cases

--- test/treescriptify/test_file_tree.py
+++ test/treescriptify/test_file_tree.py
@@ -3,7 +3,9 @@
 from treescriptify import generate_tree
 from treescriptify.data.tree_node_data import TreeNodeData
 
-from test.treescriptify.conftest import DEFAULT_INPUT, EXCLUDE_HIDDEN_INPUT, DIR_ONLY_INPUT, DIR_ONLY_HIDE_INPUT, PRUNE_DIR_INPUT, DIR_ONLY_PRUNE_INPUT, EXCLUDE_HIDDEN_WITH_DIR_ONLY_PRUNE, DEPTH_1_INPUT, DEPTH_2_INPUT
+from test.treescriptify.conftest import DEFAULT_INPUT, EXCLUDE_HIDDEN_INPUT, DIR_ONLY_INPUT, DIR_ONLY_HIDE_INPUT, \
+    PRUNE_DIR_INPUT, DIR_ONLY_PRUNE_INPUT, EXCLUDE_HIDDEN_WITH_DIR_ONLY_PRUNE, DEPTH_1_INPUT, DEPTH_2_INPUT, \
+    DEPTH_1_DIR_ONLY_INPUT, DEPTH_2_DIR_ONLY_INPUT, DEPTH_1_PRUNE_DIR_INPUT, DEPTH_2_PRUNE_DIR_INPUT
 
 
 # BASIC_TREE
@@ -250,3 +252,54 @@ def test_tree_depth_2_input_hidden_tree2_returns_data(mock_hidden_tree2):
         TreeNodeData(1, False, 'dependabot.yml'),
         TreeNodeData(0, False, '.hidden.txt'),
     ]
+
+
+def test_tree_depth_1_dir_only_input_empty_dirs_tree_returns_data(mock_empty_dirs_tree):
+    result = [x for x in generate_tree(DEPTH_1_DIR_ONLY_INPUT, mock_empty_dirs_tree)]
+    assert result == [
+        TreeNodeData(0, True, 'empty_dirs'),
+        TreeNodeData(1, True, 'dir1'),
+        TreeNodeData(1, True, 'dir2'),
+        TreeNodeData(1, True, 'dir3'),
+    ]
+
+
+def test_tree_depth_2_dir_only_input_empty_dirs_tree_returns_data(mock_empty_dirs_tree):
+    result = [x for x in generate_tree(DEPTH_2_DIR_ONLY_INPUT, mock_empty_dirs_tree)]
+    assert result == [
+        TreeNodeData(0, True, 'empty_dirs'),
+        TreeNodeData(1, True, 'dir1'),
+        TreeNodeData(1, True, 'dir2'),
+        TreeNodeData(1, True, 'dir3'),
+    ]
+
+
+def test_tree_depth_1_prune_input_empty_dirs_tree_returns_data(mock_empty_dirs_tree):
+    result = [x for x in generate_tree(DEPTH_1_PRUNE_DIR_INPUT, mock_empty_dirs_tree)]
+    assert result == [
+        TreeNodeData(0, True, 'empty_dirs'),
+    ]
+
+
+def test_tree_depth_2_prune_input_empty_dirs_tree_returns_data(mock_empty_dirs_tree):
+    result = [x for x in generate_tree(DEPTH_2_PRUNE_DIR_INPUT, mock_empty_dirs_tree)]
+    assert result == [
+        TreeNodeData(0, True, 'empty_dirs'),
+    ]
+
+
+def test_tree_depth_1_dir_only_input_hidden_tree2_returns_data(mock_hidden_tree2):
+    result = [x for x in generate_tree(DEPTH_1_DIR_ONLY_INPUT, mock_hidden_tree2)]
+    assert result == [
+        TreeNodeData(0, True, '.github'),
+        TreeNodeData(1, True, 'borkflows'),
+    ]
+
+
+def test_tree_depth_2_dir_only_input_hidden_tree2_returns_data(mock_hidden_tree2):
+    result = [x for x in generate_tree(DEPTH_2_DIR_ONLY_INPUT, mock_hidden_tree2)]
+    assert result == [
+        TreeNodeData(0, True, '.github'),
+        TreeNodeData(1, True, 'borkflows'),
+    ]
+
